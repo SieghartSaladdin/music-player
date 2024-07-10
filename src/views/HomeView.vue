55cbd31b71a8b68e13901e68911f3814e8b7401f
SieghartSaladdin <rofikhakim12@gmail.com>
2024-07-10 16:22:47 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,113 @@
+<script setup>
+import HomeCard from '@/components/HomeCard.vue';
+</script>
+
+<template>
+    <div class="p-8">
+        <button
+            type="button"
+            class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+        >
+            Podcast to make you smarter
+        </button>
+
+        <div class="py-1.5"></div>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+            <HomeCard 
+                image="https://picsum.photos/id/20/300/300" 
+                title="Title is here" 
+                subTitle="Subtitle is here"
+            />
+
+            <HomeCard 
+                image="https://picsum.photos/id/25/300/300" 
+                title="Title is here" 
+                subTitle="Subtitle is here"
+            />
+
+            <HomeCard 
+                image="https://picsum.photos/id/30/300/300" 
+                title="Title is here" 
+                subTitle="Subtitle is here"
+            />
+
+            <HomeCard 
+                image="https://picsum.photos/id/35/300/300" 
+                title="Title is here" 
+                subTitle="Subtitle is here"
+            />
+        </div>
+    </div>
+    
+    <div class="p-8">
+        <button
+            type="button"
+            class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+        >
+            Recomended Radio
+        </button>
+
+        <div class="py-1.5"></div>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+            <HomeCard 
+                image="https://picsum.photos/id/20/300/300" 
+                title="Title is here" 
+                subTitle="Subtitle is here"
+            />
+
+            <HomeCard 
+                image="https://picsum.photos/id/25/300/300" 
+                title="Title is here" 
+                subTitle="Subtitle is here"
+            />
+
+            <HomeCard 
+                image="https://picsum.photos/id/30/300/300" 
+                title="Title is here" 
+                subTitle="Subtitle is here"
+            />
+
+            <HomeCard 
+                image="https://picsum.photos/id/35/300/300" 
+                title="Title is here" 
+                subTitle="Subtitle is here"
+            />
+        </div>
+    </div>
+
+    <div class="p-8">
+        <button
+            type="button"
+            class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+        >
+            Recomended Songs
+        </button>
+
+        <div class="py-1.5"></div>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+            <HomeCard 
+                image="https://picsum.photos/id/20/300/300" 
+                title="Title is here" 
+                subTitle="Subtitle is here"
+            />
+
+            <HomeCard 
+                image="https://picsum.photos/id/25/300/300" 
+                title="Title is here" 
+                subTitle="Subtitle is here"
+            />
+
+            <HomeCard 
+                image="https://picsum.photos/id/30/300/300" 
+                title="Title is here" 
+                subTitle="Subtitle is here"
+            />
+
+            <HomeCard 
+                image="https://picsum.photos/id/35/300/300" 
+                title="Title is here" 
+                subTitle="Subtitle is here"
+            />
+        </div>
+    </div>
+</template>
---
+++ src/views/SearchView.vue
@@ -0,0 +1,58 @@
+<script setup>
+import CategorySelect from '@/components/CategorySelect.vue';
+</script>
+
+<template>
+    <div class="p-8">
+        <button
+            type="button"
+            class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+        >
+            Recomended Songs
+        </button>
+
+        <div class="py-1.5"></div>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+            <CategorySelect 
+                image="https://picsum.photos/id/40/300/300"
+                category="Podcasts" 
+            />
+            <CategorySelect 
+                image="https://picsum.photos/id/45/300/300"
+                category="New Release" 
+            />
+            <CategorySelect 
+                image="https://picsum.photos/id/50/300/300"
+                category="Audio Books" 
+            />
+            <CategorySelect 
+                image="https://picsum.photos/id/55/300/300"
+                category="Anime Songs" 
+            />
+            <CategorySelect 
+                image="https://picsum.photos/id/60/300/300"
+                category="Mushoku Tensei" 
+            />
+            <CategorySelect 
+                image="https://picsum.photos/id/36/300/300"
+                category="Podcasts" 
+            />
+            <CategorySelect 
+                image="https://picsum.photos/id/17/300/300"
+                category="New Release" 
+            />
+            <CategorySelect 
+                image="https://picsum.photos/id/19/300/300"
+                category="Audio Books" 
+            />
+            <CategorySelect 
+                image="https://picsum.photos/id/22/300/300"
+                category="Anime Songs" 
+            />
+            <CategorySelect 
+                image="https://picsum.photos/id/33/300/300"
+                category="Mushoku Tensei" 
+            />
+        </div>
+    </div>
+</template>
